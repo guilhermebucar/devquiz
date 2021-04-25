903e4977c597e38ab576e24955eff7d2a4fe5584
Guilherme Bucar <guilhermebucar@gmail.com>
2021-04-25 10:05:52 -0300

    Finalizando aula 2

--- lib/home/widgets/level_button/level_button_widget.dart
+++ lib/home/widgets/level_button/level_button_widget.dart
@@ -32,20 +32,22 @@ class LevelButtonWidget extends StatelessWidget {
     },
   };
 
+  Color get color => config[label]!['color']!;
+  Color get borderColor => config[label]!['borderColor']!;
+  Color get fontColor => config[label]!['fontColor']!;
+
   @override
   Widget build(BuildContext context) {
     return Container(
         decoration: BoxDecoration(
-          color: AppColors.levelButtonFacil,
-          border: Border.fromBorderSide(
-              BorderSide(color: AppColors.levelButtonBorderFacil)),
+          color: color,
+          border: Border.fromBorderSide(BorderSide(color: borderColor)),
           borderRadius: BorderRadius.circular(28),
         ),
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 26, vertical: 6),
           child: Text(label,
-              style: GoogleFonts.notoSans(
-                  color: AppColors.levelButtonTextFacil, fontSize: 13)),
+              style: GoogleFonts.notoSans(color: fontColor, fontSize: 13)),
         ));
   }
 }
